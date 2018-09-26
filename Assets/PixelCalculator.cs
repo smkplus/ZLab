30f3974c01a819de77291539f17ac2561bae3a7d
smkplus <smkplus3d@gmail.com>
2018-09-27 00:16:04 +0330

    Adding ZTEST Operators

--- Assets/PixelCalculator.cs
+++ Assets/PixelCalculator.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PixelCalculator : MonoBehaviour {
 	public int ScreenIndex;
 	public Transform ScreenA,ScreenB,ScreenC;
+	
 
 	// Use this for initialization
 	void Start () {
@@ -23,13 +24,28 @@ public class PixelCalculator : MonoBehaviour {
 		print(pixelA);
 		int pixelC = ScreenC.GetChild(x).GetComponent<Pixel>().index;
 		if(pixelA != 0 || pixelB != 0 ){
-		int Index = (pixelA <= pixelB) ? 1 : 0;
+		int Index = GetIndex(pixelA,pixelB);
 		Color color = PixelManager.Instance.color[Index];
 		ScreenC.GetChild(x).GetComponent<Pixel>().index = Pixels[Index];
 		ScreenC.GetChild(x).GetComponentInChildren<TextMeshProUGUI>().text = Pixels[Index].ToString();
 		ScreenC.GetChild(x).GetComponent<Image>().color = color;
 		}
 		}
+		}
+
+		int GetIndex(int pixelA,int pixelB){
+			switch (PixelManager.Instance.OperatorIndex)
+			{
+				case 0 : return (Mathf.Max(pixelA,pixelB)) == pixelB ? 1 : 0; break;
+				case 1 : return (pixelA < pixelB) ? 1 : 0; break;
+				case 2 : return (pixelA > pixelB) ? 1 : 0; break;
+				case 3 : return (pixelA <= pixelB) ? 1 : 0; break;
+				case 4 : return (pixelA >= pixelB) ? 0 : 1; break;
+				case 5 : return (pixelA == pixelB) ? 1 : 0; break;
+				case 6 : return (pixelA != pixelB) ? 1 : 0; break;
+				case 7 : return (Mathf.Max(pixelA,pixelB)) == pixelB ? 1 : 0; break;
+			}
+			return 0;
 		}
 
 
--- Assets/PixelManager.cs
+++ Assets/PixelManager.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PixelManager : Singleton<PixelManager> {
 	public Color[] color = new Color[2];
+	public int OperatorIndex;
+	public Dropdown dropdown;
 
+
+	public void SelectOperator(){
+		OperatorIndex = dropdown.value;
+		print(OperatorIndex);
+	}
 }
